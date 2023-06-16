bf194d3c9a6b4895501bf51cc61c42eef5967da6
Ghalia Zineb Hub <zamranisouhli@gmail.com>
2023-06-16 02:41:20 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print numbers of base 16 in lowercase
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	int digit = 48; /*48: decimal rep of 0*/
+
+	while (digit <= 102) /*102: desimal rep of f*/
+	{
+		putchar(digit);
+		/*after 9 we jump till 96*/
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar("\n");
+	return (0);
+}
