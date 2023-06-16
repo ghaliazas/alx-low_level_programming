79298f8b7a397b1ab49021c12638dce1c73ca245
Ghalia Zineb Hub <zamranisouhli@gmail.com>
2023-06-16 02:05:56 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print all alphabet letters
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
