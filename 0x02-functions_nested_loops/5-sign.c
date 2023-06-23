e13de684f87faf4ebb1cf4fb4600692ca55ab3f9
Ghalia Zineb Hub <zamranisouhli@gmail.com>
2023-06-22 17:34:56 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - print + if n is greater than zero.
  *              0 if n is zero - and if n is less
  *              then zero
- * 
+ *
  * @c: takes integer type input for function.
  *
  * Return: 1 if +. 0 if 0 and -1 if -
@@ -21,10 +21,10 @@ int print_sign(int n)
 	{
 		_putchar(48);
 		return (0);
-        }
+	}
 	else
-        {
+	{
 		_putchar(45);
 		return (-1);
-        }
+	}
 }
