454f1a87270d6e1a41b6def014e408d1ca53bfde
Ghalia Zineb Hub <zamranisouhli@gmail.com>
2023-07-06 03:27:53 -0700

    0x08. C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,9 +4,9 @@
 /**
  * _strlen - Calculates the length of a string.
  * @s: The string to calculate the length of.
- *
  * Return: The length of the string.
  */
+
 int _strlen(char *s)
 {
 	if (*s == '\0')
@@ -16,11 +16,29 @@ int _strlen(char *s)
 }
 
 /**
+ * check_palindrome - Recursive helper function
+ * to check if a string is a palindrome.
+ * @s: The string to check.
+ * @start: The starting index of the current substring.
+ * @end: The ending index of the current substring.
+ * Return: 1 if the substring is a palindrome, 0 otherwise.
+ */
+
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to check.
- *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
+
 int is_palindrome(char *s)
 {
 	int len = _strlen(s);
