dfd142d1cab07a7bbf4439ae6b99cd15bca4cd55
Ghalia Zineb Hub <zamranisouhli@gmail.com>
2023-07-13 07:35:29 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,48 +1,49 @@
 #include "main.h"
 
 /**
+ * _strlen - finds length of a string
+ * @s: string
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+
+/**
  * argstostr - Concatenates all the arguments of a program.
  * @ac: The number of arguments.
  * @av: An array of strings containing the arguments.
- * Return: On success, returns a pointer to the newly
- * allocated concatenated string.
- * Returns NULL if ac is 0 or if av is NULL, or if memory allocation fails.
+ * Return: Success, returns a pointer to newly allocated concatenated string
+ *         and NULL if ac is 0 or if av is NULL, or if memory allocation fails
  */
 
 char *argstostr(int ac, char **av)
 {
-	char *concat;
-	int i, j, k, len, total_len = 0;
+	int i = 0, nc = 0, j = 0, cmpt = 0;
+	char *s;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+	for (; i < ac; i++, nc++)
+		nc += _strlen(av[i]);
 
-	for (i = 0; i < ac; i++)
-	{
-		len = 0;
-		while (av[i][len] != '\0')
-		{
-			len++;
-			total_len++;
-		}
-		total_len++; /* Account for newline character */
-	}
-
-	concat = malloc(sizeof(char) * (total_len + 1));
-	if (concat == NULL)
+	s = malloc(sizeof(char) * nc + 1);
+	if (s == 0)
 		return (NULL);
-
-	k = 0;
 	for (i = 0; i < ac; i++)
 	{
-		len = 0;
-		while (av[i][len] != '\0')
-		{
-			concat[k++] = av[i][len++];
-		}
-		concat[k++] = '\n';
-	}
-	concat[k] = '\0';
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+			s[cmpt] = av[i][j];
 
-	return (concat);
+		s[cmpt] = '\n';
+		cmpt++;
+	}
+	s[cmpt] = '\0';
+	return (s);
 }
