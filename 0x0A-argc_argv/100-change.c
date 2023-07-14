613c0ad4237ed56d2b85cb1e2327dce11d7828e2
Ghalia Zineb Hub <zamranisouhli@gmail.com>
2023-07-14 03:59:11 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,56 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - prints the minimum number of coins
- * to make change for an amount of money
- * @argc: number of commandline arguments
- * @argv: pointer to an array of command line arguments
- * return: 0 - success, non-zero-fail
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
-	cents = atoi(argv[1]);
-	
-	if (cents < 0)
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 
-	while (cents > 0)
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		if (cents >= 25)
+		while (num >= coins[j])
 		{
-			cents -= 25;
+			result++;
+			num -= coins[j];
 		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-		}
-		else
-		{
-			cents -= 1;
-		}
-		coins++;
 	}
-	printf("%d\n", coins);
-	return 0;
+
+	printf("%d\n", result);
+	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints the number of arguments passed into the program
- * @argc: int
- * @argv: list
- * return: 0
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
+
 	return (0);
 }
